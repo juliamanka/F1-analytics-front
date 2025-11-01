30159f835004ed82f8caa1a7e6fc775483c42485
juliamanka <juliamanka001@gmail.com>
2025-11-01 16:47:46 +0100

    use correct base api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  //apiBaseUrl: 'https://f1-api-d0gpdbfdejdefkb9.polandcentral-01.azurewebsites.net/api',
-  apiBaseUrl: 'http://localhost:5288/api'
+  apiBaseUrl: 'https://f1-api-d0gpdbfdejdefkb9.polandcentral-01.azurewebsites.net/api'
 };
